8acbf0a8e575487a38dc62576889675a15d46320
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-05-25 09:10:11 -0500

    added some routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,101 @@
+const router = require("express").Router();
+const { post } = require(".");
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+// get all posts for homepage
+router.get("/", (req, res) => {
+  post
+    .findAll({
+      attributes: ["id", "post_text", "title", "created_at"],
+      order: [["created_at", "DESC"]],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    })
+
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    });
+});
+
+// get single post
+router.get("/login", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get all posts for dashboard
+
+router.get("/posts-comments", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/posts-comments");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+module.exports = router;

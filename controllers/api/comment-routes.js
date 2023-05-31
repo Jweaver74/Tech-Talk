4b8d55139b4ead64a22a2b14cc4a8c8d43967e85
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-05-31 15:20:02 -0500

    fixed the delete buttom

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -27,21 +27,6 @@ router.post("/", withAuth, (req, res) => {
   }
 });
 
-// POST /api/comments
-router.post("/", withAuth, (req, res) => {
-  if (req.session) {
-    Comment.create({
-      comment_text: req.body.comment_text,
-      user_id: req.session.user_id,
-      post_id: req.body.post_id,
-    })
-      .then((dbCommentData) => res.json(dbCommentData))
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  }
-});
 
 // DELETE /api/comments/1
 router.delete("/:id", withAuth, (req, res) => {
